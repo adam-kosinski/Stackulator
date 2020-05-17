953123e00ccd5622cbf7c137be1087ab524dcd75
Adam <myemail>
2020-05-17 02:42:05 -0400

    Finished stackulator

--- Stackulator.java
+++ Stackulator.java
@@ -3,6 +3,7 @@ import javafx.application.Platform;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
+import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -23,7 +24,7 @@ public class Stackulator extends Application
 	{
 		stack = new AStack<Double>();
 		display = new Text();
-		stack_display = new Text(stack.toString());
+		stack_display = new Text("Stack: " + stack.toString());
 		showing_result = false;
 		isRadians = true;
 	}
@@ -31,23 +32,52 @@ public class Stackulator extends Application
 	public void start(Stage stage)
 	{
 		BorderPane bp = new BorderPane();
+		bp.setId("main");
 		
 		//stuff on the top
-		HBox top = new HBox();
+		VBox top = new VBox();
 		top.setId("top");
 		bp.setTop(top);
 		top.getChildren().add(display);
-		
-		//other
-		bp.setBottom(stack_display);
+		stack_display.setId("stack_display");
+		top.getChildren().add(stack_display);
 		
 		//button layout preparation
-		GridPane left = new GridPane();
-		GridPane right = new GridPane();
+		GridPane left = new GridPane(); //contains number pad, operations
+		GridPane right = new GridPane(); //contains functions
+		left.getStyleClass().addAll("side","gridpane");
+		right.getStyleClass().addAll("side","gridpane");
+		
+		//set up left side
+		GridPane numbers_and_above = new GridPane();
+		numbers_and_above.getStyleClass().add("gridpane");
+		GridPane above_numbers = new GridPane();
+		GridPane number_pad = new GridPane();
+		above_numbers.getStyleClass().add("gridpane");
+		number_pad.getStyleClass().add("gridpane");
+		numbers_and_above.add(above_numbers, 0, 0);
+		numbers_and_above.add(number_pad, 0, 1);
+		left.add(numbers_and_above, 0, 0);
+		
+		GridPane operations = new GridPane();
+		operations.getStyleClass().add("gridpane");
+		left.add(operations, 1, 0);
+		
+		//spacer
+		VBox spacer = new VBox();
+		spacer.setMinWidth(10);
+		
+		//set up right side
+		GridPane function_pad = new GridPane();
+		right.add(function_pad, 0, 0);
+		
+		//put everything in
 		GridPane container = new GridPane();
 		bp.setCenter(container);
 		container.add(left, 0, 0); //args: child, col_idx, row_idx
-		container.add(right, 1, 0);
+		container.add(spacer, 1, 0);
+		container.add(right, 2, 0);
+		function_pad.getStyleClass().add("gridpane");
 		
 		
 		//make number buttons
@@ -62,7 +92,7 @@ public class Stackulator extends Application
 		//add number buttons		
 		int i = 0;
 		for (char c : "789456123.0E".toCharArray()) {
-			left.add(new NumberButton("" + c), i % 3, i / 3);
+			number_pad.add(new NumberButton("" + c), i % 3, i / 3);
 			i++;
 		}
 		
@@ -74,7 +104,7 @@ public class Stackulator extends Application
 			if(!Double.isNaN(d))
 			{
 				stack.push(d);
-				stack_display.setText(stack.toString());
+				stack_display.setText("Stack: " + stack.toString());
 				display.setText("");
 			}
 			else
@@ -82,24 +112,13 @@ public class Stackulator extends Application
 				display.setText("ERROR");
 			}
 		});
+		enter.setId("enter");
 		enter.getStyleClass().add("button");
-		right.add(enter, 0, 0);
+		enter.setMinWidth(105);
+		enter.setMaxWidth(105);
+		above_numbers.add(enter, 0, 0);
 		
-		//clear buttons
-		Button clear = new Button("C");
-		clear.setOnAction( e ->
-		{
-			stack.clear();
-			display.setText("");
-			stack_display.setText(stack.toString());
-			showing_result = false;
-		});
-		Button clear_entry = new Button("CE");
-		clear_entry.setOnAction( e ->
-		{
-			display.setText("");
-			showing_result = false;
-		});
+		//backspace
 		Button backspace = new Button("<-");
 		backspace.setOnAction( e ->
 		{
@@ -110,27 +129,48 @@ public class Stackulator extends Application
 				showing_result = false;
 			}
 		});
-		clear.getStyleClass().add("button");
-		clear_entry.getStyleClass().add("button");
+		backspace.setId("backspace");
 		backspace.getStyleClass().add("button");
+		backspace.setMinWidth(50);
+		backspace.setMaxWidth(50);
+		above_numbers.add(backspace, 1, 0);
+
 		
-		right.add(clear, 1, 0);
-		right.add(clear_entry, 1, 1);
-		right.add(backspace, 1, 2);
+		//clear buttons
+		Button clear = new Button("C");
+		clear.setOnAction( e ->
+		{
+			stack.clear();
+			display.setText("");
+			stack_display.setText("Stack: " + stack.toString());
+			showing_result = false;
+		});
+		Button clear_entry = new Button("CE");
+		clear_entry.setOnAction( e ->
+		{
+			display.setText("");
+			showing_result = false;
+		});
+		clear.getStyleClass().addAll("button","clear_button");
+		clear.setMinWidth(65);
+		clear_entry.getStyleClass().addAll("button","clear_button");
+		clear_entry.setMinWidth(65);
+		function_pad.add(clear, 0, 0);
+		function_pad.add(clear_entry, 0, 1);
 		
 		//radian/deg mode button
-		Button rad_deg = new Button("to deg");
+		Button rad_deg = new Button("RAD");
 		rad_deg.setOnAction( e ->
 		{
 			if(isRadians)
 			{
 				isRadians = false;
-				rad_deg.setText("to rad");
+				rad_deg.setText("DEG");
 			}
 			else
 			{
 				isRadians = true;
-				rad_deg.setText("to deg");
+				rad_deg.setText("RAD");
 			}
 		});
 		rad_deg.getStyleClass().add("button");
@@ -140,26 +180,27 @@ public class Stackulator extends Application
 		FunctionButton log10 = new FunctionButton("log10", x -> Math.log10(x));
 		FunctionButton sqrt = new FunctionButton("sqrt", x -> Math.sqrt(x));
 		FunctionButton negate = new FunctionButton("(-)", x -> -x);
-		FunctionButton sin = new FunctionButton("sin", x ->
-		{
-			return isRadians ? Math.sin(x) : Math.sin(Math.toRadians(x));
-		});
-		FunctionButton asin = new FunctionButton("asin", x ->
-		{
-			return isRadians ? Math.asin(x) : Math.toDegrees(Math.asin(x));
-		});
-		FunctionButton cos = new FunctionButton("cos", Math::cos);
-		FunctionButton tan = new FunctionButton("tan", Math::tan);
 		
-		right.add(rad_deg, 3, 0);
-		right.add(ln, 2, 0);
-		right.add(log10, 2, 1);
-		right.add(sqrt, 2, 2);
-		right.add(negate, 2, 3);
-		right.add(sin, 2, 4);
-		right.add(asin, 2, 5);
-		right.add(cos, 2, 6);
-		right.add(tan, 2, 7);
+		FunctionButton sin = new FunctionButton("sin", x -> {return isRadians ? Math.sin(x) : Math.sin(Math.toRadians(x));});
+		FunctionButton asin = new FunctionButton("asin", x -> {return isRadians ? Math.asin(x) : Math.toDegrees(Math.asin(x));});
+		
+		FunctionButton cos = new FunctionButton("cos", x -> {return isRadians ? Math.cos(x) : Math.cos(Math.toRadians(x));});
+		FunctionButton acos = new FunctionButton("acos", x -> {return isRadians ? Math.acos(x) : Math.toDegrees(Math.acos(x));});
+		
+		FunctionButton tan = new FunctionButton("tan", x -> {return isRadians ? Math.tan(x) : Math.tan(Math.toRadians(x));});
+		FunctionButton atan = new FunctionButton("atan", x -> {return isRadians ? Math.atan(x) : Math.toDegrees(Math.atan(x));});
+		
+		function_pad.add(negate, 2, 0);
+		function_pad.add(sqrt, 2, 1);
+		function_pad.add(ln, 2, 2);
+		function_pad.add(log10, 2, 3);
+		function_pad.add(rad_deg, 3, 0);
+		function_pad.add(sin, 3, 1);
+		function_pad.add(asin, 4, 1);
+		function_pad.add(cos, 3, 2);
+		function_pad.add(acos, 4, 2);
+		function_pad.add(tan, 3, 3);
+		function_pad.add(atan, 4, 3);
 		
 		//op buttons
 		OpButton plus = new OpButton("+", (a,b) -> a+b);
@@ -167,18 +208,15 @@ public class Stackulator extends Application
 		OpButton times = new OpButton("*", (a,b) -> a*b);
 		OpButton divide = new OpButton("/", (a,b) -> a/b);
 		OpButton pow = new OpButton("^", (a,b) -> Math.pow(a,b));
-		right.add(plus, 0, 1);
-		right.add(minus, 0, 2);
-		right.add(times, 0, 3);
-		right.add(divide, 0, 4);
-		right.add(pow, 0, 5);
+		operations.add(plus, 0, 0);
+		operations.add(minus, 0, 1);
+		operations.add(times, 0, 2);
+		operations.add(divide, 0, 3);
+		operations.add(pow, 0, 4);
 		
-		left.getStyleClass().add("gridpane");
-		right.getStyleClass().add("gridpane");
+
 		
-		bp.setId("main");
-		
-		Scene scene = new Scene(bp, 800, 500);
+		Scene scene = new Scene(bp, 650, 400);
 		scene.getStylesheets().add("./stackulator.css");
 		stage.setScene(scene);
 		stage.show();
@@ -210,6 +248,7 @@ public class Stackulator extends Application
 			
 			this.getStyleClass().addAll("button","number_button");
 			this.setMinWidth(50);
+			this.setMaxWidth(50);
 			
 			this.symbol = symbol;
 			setOnAction( e ->
@@ -220,7 +259,7 @@ public class Stackulator extends Application
 					if(!Double.isNaN(d))
 					{
 						stack.push(getDisplayValue());
-						stack_display.setText(stack.toString());
+						stack_display.setText("Stack: " + stack.toString());
 					}
 					display.setText("");
 				}
@@ -238,6 +277,8 @@ public class Stackulator extends Application
 			super(symbol);
 			
 			this.getStyleClass().addAll("button","op_button");
+			this.setMinWidth(50);
+			this.setMaxWidth(50);
 			
 			this.op = op;
 			setOnAction( e ->
@@ -257,7 +298,7 @@ public class Stackulator extends Application
 					return;
 				}
 				double result = op.applyAsDouble(first, second);
-				stack_display.setText(stack.toString());
+				stack_display.setText("Stack:" + stack.toString());
 				display.setText("" + result);
 				showing_result = true;
 			});
